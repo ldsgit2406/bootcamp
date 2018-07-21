5c8f841043281cc73e9e3eca6b23d5c65c1d9837
ldsgit2406 <Dhanasekar@DESKTOP-RK2B4CQ>
2018-07-22 05:28:36 +0530

    commit for block chain boot camp 1 implementation

--- java/src/main/java/Blockchain.java
+++ java/src/main/java/Blockchain.java
@@ -5,62 +5,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Blockchain {
-    private List blockchain = new ArrayList<Block>();
+	private List<Block> blockchain = new ArrayList<Block>();
 
-    public void add_blocks(Block blk){
-        this.blockchain.add(blk);
-    }
+	public void add_blocks(Block blk) {
+		this.blockchain.add(blk);
+	}
 
-    public Block get_latest_block(){
-        return (Block) this.blockchain.get(blockchain.size()-1);
-    }
+	public Block get_latest_block() {
+		return (Block) this.blockchain.get(blockchain.size() - 1);
+	}
 
-    public boolean validateChain(){
-        return false;
+	public boolean validateChain() {
+		// return false;
 
-        //TODO implement this,
-        /*
-        1. Iterate over all blocks and validate()
-        2. Check if the prev_hash of current block is same as the hash
-        of previous block
-        block.prev_hash == previous_block.hash
-         */
-    }
+		// TODO implement this,
+		/*
+		 * 1. Iterate over all blocks and validate() 2. Check if the prev_hash
+		 * of current block is same as the hash of previous block
+		 * block.prev_hash == previous_block.hash
+		 */
+		String prevHash = null;
+		for (Block b : this.blockchain) {
 
-    @Override
-    public String toString() {
-        Gson gson = new Gson();
-        return gson.toJson(this);
-    }
+			if (!b.validate()
+					|| (prevHash != null && prevHash
+							.equals(b.getPrev_hash()))) {
+				return false;
+			}
 
-    public static Blockchain blockchainFromJson(String json){
-        Gson gson = new Gson();
-        return gson.fromJson(json, Blockchain.class);
-    }
-    public static Blockchain blockchainFromJson(BufferedReader json){
-        Gson gson = new Gson();
-        return gson.fromJson(json, Blockchain.class);
-    }
+			prevHash = b.getHash();
+		}
 
-    public void saveAsJson(){
-        try {
-            FileWriter writer = new FileWriter("blockchain.json");
-            writer.write(this.toString());
-            writer.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		return true;
+	}
 
-    public static Blockchain loadFromJson(){
-        try {
-            BufferedReader br = new BufferedReader(
-                    new FileReader("blockchain.json"));
-            return blockchainFromJson(br);
+	@Override
+	public String toString() {
+		Gson gson = new Gson();
+		return gson.toJson(this);
+	}
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        return null;
-        }
+	public static Blockchain blockchainFromJson(String json) {
+		Gson gson = new Gson();
+		return gson.fromJson(json, Blockchain.class);
+	}
+
+	public static Blockchain blockchainFromJson(BufferedReader json) {
+		Gson gson = new Gson();
+		return gson.fromJson(json, Blockchain.class);
+	}
+
+	public void saveAsJson() {
+		try {
+			FileWriter writer = new FileWriter("blockchain.json");
+			writer.write(this.toString());
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Blockchain loadFromJson() {
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(
+					"blockchain.json"));
+			return blockchainFromJson(br);
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
